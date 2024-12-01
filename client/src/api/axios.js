e7ba412229c5a143cf5d27daa89718ad6235eaa4
Alejandro <alejandrorodriguezalcala1@gmail.com>
2024-12-01 23:43:00 +0000

    Axios API_URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: API_URL,
   withCredentials: true
 })
 
