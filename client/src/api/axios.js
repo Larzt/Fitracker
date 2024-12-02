c365b433b408a1dc76b1675a28c85bf0db458d73
Alejandro <alejandrorodriguezalcala1@gmail.com>
2024-12-02 00:20:14 +0000

    Fix URLs

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 // import dotenv from 'dotenv';
 // dotenv.config();
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+const API_URL = 'https://fitracker-h0at.onrender.com'
 
 const instance = axios.create({
   baseURL: API_URL,
